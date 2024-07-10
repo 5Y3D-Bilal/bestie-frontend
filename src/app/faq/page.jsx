68f57a431d90fb271f5733bae6e5a01959e866c6
5Y3D-Bilal <54587dfdd@gmail.com>
2024-07-10 12:33:44 -0700

    added FAQ pgae and installed frammer motion

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import FAQQuestions from "../../components/FAQ/FAQQuestions";
 
 function page() {
   return (
     <div>
-      
+      <FAQQuestions />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
