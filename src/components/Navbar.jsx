ae7028d2d2d3b6d1cb19e492d434f23039744586
5Y3D-Bilal <54587dfdd@gmail.com>
2024-08-31 00:48:04 +0500

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -105,7 +105,7 @@ function Navbar() {
                     {currentUser?.storeId ? (
                       <Link href={'/post'} className="flex justify-center items-center w-24 h-10 lg:w-28 lg:h-12 bg-white cursor-pointer rounded-3xl border-2 border-[#9748FF] shadow-[inset_0px_-2px_0px_1px_#9748FF] group hover:bg-[#9748FF] transition duration-300 ease-in-out">
                         <span className="font-medium text-[#333] group-hover:text-white">
-                          Post Ads
+                          Post Ad
                         </span>
                       </Link>
                     ) : (
